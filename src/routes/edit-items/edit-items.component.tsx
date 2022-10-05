804b534cc15c305c5dc0b8c85df09ac9af2da6c4
unknown <adu.cohan@gmail.com>
2022-10-04 18:25:52 -0700

    added more to item editing screen

--- src/routes/edit-items/edit-items.component.tsx
+++ src/routes/edit-items/edit-items.component.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
-import { Container, ListGroup, Badge, Col, Row, Form } from "react-bootstrap";
+import { ChangeEvent, useEffect, useState } from "react";
+import { Container, ListGroup, Badge, Col, Row, Form, InputGroup, Button } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
+import { Input } from "../../components/form-input/form-input.styles";
 import { fetchProductsStart } from "../../store/products/products.action";
 import { selectProducts } from "../../store/products/products.selector";
 
@@ -9,13 +10,26 @@ import { Product } from "../../store/products/products.types";
 const EditItems = () => {
     const items = useSelector(selectProducts);
     const dispatch = useDispatch();
-    const [selectedItem, setSelectedItem] = useState<Product>()
+    const [selectedItem, setSelectedItem] = useState( {} as Product);
 
     const checkIsActive = (item:Product) => {
-        if (item === selectedItem) {
+        if (item["Item GUID"] === selectedItem["Item GUID"]) {
             return true;
         }
         else return false;
+    }
+
+    const handleFormChange = (event: ChangeEvent<HTMLInputElement>) => {
+        console.log(event.target)
+        const { name, value} = event.target;
+        setSelectedItem({...selectedItem, [name]:value})
+        console.log(selectedItem)
+    }
+    const handleToggle = (e: ChangeEvent) => setSelectedItem({...selectedItem,"Enabled":!selectedItem.Enabled});
+
+    const checkForChanges = () => {
+        const currentlySelected = items.find((element) => element["Item GUID"]===selectedItem["Item GUID"])
+        return(selectedItem === currentlySelected)
     }
 
     useEffect(() => {
@@ -33,8 +47,10 @@ const EditItems = () => {
                                 a['Sort Order'] > b['Sort Order'] ? 1 : -1,
                                 ).map(
                                 (item) =>
-                                    <ListGroup.Item className="d-flex justify-content-between align-items-start"
-                                        variant="primary"
+                                    <ListGroup.Item 
+                                        className="d-flex justify-content-between align-items-start"
+                                        key={item["Item GUID"]}
+                                        variant={item.Enabled?"primary":"secondary"}
                                         active={checkIsActive(item)}
                                         // disabled={!item.Enabled}
                                         action onClick={() => setSelectedItem(item)}
@@ -47,21 +63,87 @@ const EditItems = () => {
                     </ListGroup>
                     </Col>
                     <Col>
-                        {selectedItem? (
-                            //add onchange event for these form fields to update selectedItem. Then we can patch with a save button
+                        {selectedItem["Item GUID"]? (
                             <Form>
-                                <Form.Text>Item</Form.Text>
+                                    <InputGroup>
+                                        <Form.Check
+                                            onChange={handleToggle}
+                                            id="Enabled"
+                                            type="switch"
+                                            checked={selectedItem.Enabled}
+                                        />
+                                        <Form.Label for="Enabled">
+                                            {selectedItem.Enabled? "Available":"Disabled"}
+                                        </Form.Label>
+                                    </InputGroup>
+                                <InputGroup>
+                                    <InputGroup.Text>Sort</InputGroup.Text>
+                                    <Form.Control 
+                                        onChange={handleFormChange} 
+                                        name="Sort Order"
+                                        type="number"
+                                        value={selectedItem["Sort Order"]} 
+                                    />
+                                </InputGroup>
+                                <Form.Text>Name</Form.Text>
+                                <Form.Control 
+                                    onChange={handleFormChange} 
+                                    name="Item Name"
+                                    value={selectedItem["Item Name"]} 
+                                />
+                                <Form.Text>Display Name</Form.Text>
+                                <Form.Control 
+                                    onChange={handleFormChange}
+                                    name="DisplayName"
+                                    value={selectedItem.DisplayName} 
+                                    />
+                                <Form.Text>Mapping</Form.Text>
+                                <InputGroup>
+                                    <InputGroup.Text>GL Code</InputGroup.Text>
+                                    <Form.Control 
+                                        onChange={handleFormChange} 
+                                        name="GL index"
+                                        value={selectedItem["GL index"]} 
+                                    />
+                                    <InputGroup.Text>Category</InputGroup.Text>
+                                    <Form.Control 
+                                        onChange={handleFormChange} 
+                                        name="Category Code"
+                                        value={selectedItem["Category Code"]} 
+                                    />
+                                </InputGroup>
+                                <Form.Text>Prices</Form.Text>
+                                <InputGroup>
+                                    <InputGroup.Text>WA</InputGroup.Text>
+                                    <InputGroup.Text>$</InputGroup.Text>
+                                    <Form.Control 
+                                        onChange={handleFormChange} 
+                                        name="List Price (WA)"
+                                        type="number"
+                                        value={selectedItem["List Price (WA)"]} 
+                                    />
+                                    <InputGroup.Text>OR</InputGroup.Text>
+                                    <InputGroup.Text>$</InputGroup.Text>
+                                    <Form.Control 
+                                        onChange={handleFormChange} 
+                                        name="List Price (OR)"
+                                        type="number"
+                                        value={selectedItem["List Price (OR)"]}
+                                    />
+                                </InputGroup>
+
+                                <Form.Text>GUID</Form.Text>
                                 <Form.Control disabled
                                     value={selectedItem["Item GUID"]}
                                 />
-                                <Form.Text>Name</Form.Text>
-                                <Form.Control value={selectedItem["Item Name"]} />
-                                <Form.Text>Display Name</Form.Text>
-                                <Form.Control value={selectedItem.DisplayName} />
-                                <Form.Text>GL Code</Form.Text>
-                                <Form.Control value={selectedItem["GL index"]} />
-                                <Form.Text>Is Active?</Form.Text>
-                                <Form.Check defaultChecked={selectedItem.Enabled} />
+                                <Form.Group as={Row}>
+                                    <Col className="mt-4 text-end">
+                                        <Button
+                                            disabled={checkForChanges()}
+                                            onClick={() => alert("this doesn't do anything yet")}
+                                        >Save Changes</Button>
+                                    </Col>
+                                </Form.Group>
                             </Form>
                             ):null
                         }
